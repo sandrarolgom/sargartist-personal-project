d9b6c1faf898c386504f95af21c0a83578ba062c
sandrarolgom <sandraroldan210900@gmail.com>
2024-10-01 15:07:08 +0200

    create partials scss, variables and reset scss

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import logo from "../images/logo1NotBack.png"; 
+function Header() {
+  return (
+    <header>
+        <h1>header</h1>
+        {/* <img src={logo} alt="logo sargartist"/> */}
+    </header>
+  )
+}
+
+export default Header
