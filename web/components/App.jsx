7e74e54d8316404244f1b8736f93cf1511324225
sandrarolgom <sandraroldan210900@gmail.com>
2024-10-08 16:25:14 +0200

    create contact styles

--- web/components/App.jsx
+++ web/components/App.jsx
@@ -6,42 +6,12 @@ import AboutMe from "./AboutMe";
 import Contact from "./Contact";
 
 function App() {
-   //Fake URL used as a illustrations images
-  const fakeImg = "https://static.vecteezy.com/system/resources/previews/023/779/824/non_2x/animal-dog-head-square-line-simple-creative-logo-free-vector.jpg";
-
-  //Illustrations array, holding sample images
-  const illustrations = [
-    {
-      img: fakeImg,
-      name: "test",
-    },
-    {
-      img: fakeImg,
-      name: "test",
-    },
-    {
-      img: fakeImg,
-      name: "test",
-    },
-    {
-      img: fakeImg,
-      name: "test",
-    },
-    {
-      img: fakeImg,
-      name: "test",
-    },
-    {
-      img: fakeImg,
-      name: "test",
-    }
-  ];
-
+  
   return (
     <>
       <Routes>
         <Route path="/" element={<Landing />} />
-        <Route path="/projects" element={<Projects arrayFake={illustrations}/>} />
+        <Route path="/projects" element={<Projects />} />
         <Route path="/aboutme" element={<AboutMe />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
--- web/components/Contact.jsx
+++ web/components/Contact.jsx
@@ -3,9 +3,13 @@ import Header from "./Header";
 
 function Contact() {
     return (
-        <div>
+        <div className="contact">
             <Header />
-            <h1>contact</h1>
+            <article className="contact__container">
+                <h1 className="contact__container--title">contact</h1>
+                <p className="contact__container--message">Hola! pronto podrás ponerte en contacto conmigo, si necesitas información puedes escribirme por instagram en el siguiente enlace:</p>
+                <a className="contact__container--link" href="https://www.instagram.com/sargartist/">@sargartist</a>
+            </article>
             <Footer />
         </div>
     );
