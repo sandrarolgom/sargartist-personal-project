159de0991175868a89b76f4a45d290e9ca83b2b7
sandrarolgom <sandraroldan210900@gmail.com>
2024-10-11 11:04:39 +0200

    add illustrations

--- bd/populateIllustrationsTable.sql
+++ bd/populateIllustrationsTable.sql
@@ -6,3 +6,8 @@ VALUES
 ("sunny", "sunny.png"),
 ("trino", "trino.png"),
 ("yeron", "yeron.png");
+INSERT INTO illustrations (name, fileName) 
+VALUES
+("kena", "kena.png"),
+("yonsi", "yonsi.png"),
+("arwen", "arwen.png");
