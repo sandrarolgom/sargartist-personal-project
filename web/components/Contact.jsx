91029d1f389059d528c9f19c9bd2c1f1b7f40e57
sandrarolgom <sandraroldan210900@gmail.com>
2024-10-13 12:36:56 +0200

    add medias contact 768px

--- web/components/Contact.jsx
+++ web/components/Contact.jsx
@@ -1,15 +1,13 @@
 
 function Contact() {
     return (
-        <div className="testContact">
-            <section className="contact">
+        <section className="contact">
             <article className="contact__container">
                 <h1 className="contact__container--title hidden">contact</h1>
                 <p className="contact__container--message">Hola! pronto podrás ponerte en contacto conmigo, si necesitas información puedes escribirme por instagram en el siguiente enlace:</p>
                 <a className="contact__container--link" href="https://www.instagram.com/sargartist/" target="_blank">@sargartist</a>
             </article>
-            </section>            
-        </div>
+        </section>            
     );
 }
 
