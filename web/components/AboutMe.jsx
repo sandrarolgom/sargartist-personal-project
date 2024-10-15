c1009aca3a704599aac703f0365547c63f5ea2f2
sandrarolgom <sandraroldan210900@gmail.com>
2024-10-15 17:11:51 +0200

    test url

--- web/components/AboutMe.jsx
+++ web/components/AboutMe.jsx
@@ -9,7 +9,7 @@ function AboutMe() {
                 Hola! Soy Sandra, ilustradora apasionada por capturar la esencia y personalidad de las mascotas a través de mi trabajo. Desde siempre he sentido una conexión muy especial por los animales y también por el arte así que decidí unir mis dos pasiones y lanzarme a realizar ilustraciones personalizadas de mascotas.
             </p>
             <p className="aboutme__text--p">
-                Para mí cada obra es como conocer a una nueva mascota por primera vez. La ilusión por la que cada persona viene a mí para poder crear ese recuerdo tan especial me hace sentirme como parte de la familia. ¡Y no hay nada que me emocione más que ver la felicidad en los rostros de quienes ven a sus peluditos plasmados en mis ilustraciones!
+                Para mí cada ilustración es como conocer a una nueva mascota por primera vez. La ilusión por la que cada persona viene a mí para poder crear ese recuerdo tan especial me hace sentirme como parte de la familia. ¡Y no hay nada que me emocione más que ver la felicidad en los rostros de quienes ven a sus peluditos plasmados en mis ilustraciones! 
             </p>
             </article>
             <img
